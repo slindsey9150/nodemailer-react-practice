ef2b914d0a6d355982436d78f95b0fdce97d6a6a
slindsey9150 <stevie.lindsey.jr@gmail.com>
2024-08-01 11:49:10 -0500

    changed the flow to only allow the user to proceed once they've correctly inputed the passwort reset code

--- src/components/ResetPasswordCodeConfForm/ResetPasswordCodeConfForm.jsx
+++ src/components/ResetPasswordCodeConfForm/ResetPasswordCodeConfForm.jsx
@@ -1,17 +1,34 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import { useSelector } from "react-redux"
+import { useState } from "react"
 
 export default function ResetPasswordCodeConfForm () {
+    const [codeAttempt, setCodeAttempt] = useState('')
+    const resetCodeStore = useSelector((store) => store.email)
+    const resetCode = resetCodeStore.emailReducer
     const history = useHistory()
 
-    const submitCode = () => {
-        history.push('/reset-password-page')
+    const submitCode = (event) => {
+        event.preventDefault()
+        console.log("resetCode", resetCode);
+        console.log("code attempt", codeAttempt);
+        if (codeAttempt == resetCode){
+            console.log("you entered the reset code correctly");
+            history.push('/reset-password-page')
+        }
+        else{
+            alert("you entered the reset code incorrectly dumbass");
+        }
+       
     }
     return(
         <div>
             <h1>Please enter your password reset code:</h1>
-            <form>
-               Code: <input></input>
-               <button onClick={submitCode}>Submit</button>
+            <form onSubmit={submitCode}>
+               Code: <input 
+               value={codeAttempt}
+               onChange={(event) => setCodeAttempt(event.target.value)}></input>
+               <button>Submit</button>
 
             </form>
         </div>
